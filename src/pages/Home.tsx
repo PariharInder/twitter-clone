b95ae3338f80a8c361ef19308b12f678e070fc2c
PariharInder <2002inderjitsingh@gmail.com>
2023-03-09 14:34:02 -0500

    added routing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,15 @@
-import logo from './assets/twitter-logo.svg'
+import logo from '../assets/twitter-logo.svg'
 
 export const Home = () => {
     return (
-        <div className='flex justify-center items-center h-screen'>
+        <div className='flex justify-center items-center h-screen dark:bg-neutral-900'>
             <div className='p-10 rounded-xl flex items-center'>
                 <div>
                     <img src={logo} alt='twitter clone logo' className='mx-auto w-2/5 m-10' />
                     <h2 className='text-3xl text-center text-white mb-4 font-extrabold'>Don't miss what's happening</h2>
                     <div className='mt-4 flex justify-center'>
                         <a href="#" className='m-4 py-2 px-4 bg-blue-500 text-white rounded-full shadow-2xl hover:bg-blue-400 font-bold'>Login</a>
-                        <a href="#" className='m-4 py-2 px-4 bg-gray-300 text-gray-800 rounded-full hover:bg-gray-200 font-bold'>SignUp</a>
+                        <a href="/signup" className='m-4 py-2 px-4 bg-gray-300 text-gray-800 rounded-full hover:bg-gray-200 font-bold'>SignUp</a>
                     </div>
                 </div>
             </div>
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,7 @@
+export const SignUp = () => {
+    return (
+        <>
+        <h1 className='text-white'>It works</h1>
+        </>
+    )
+}
